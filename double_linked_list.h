1ccdcadac02ab21f9ee9ad2d78d4022e62587f73
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2020-12-05 09:12:51 +0100

    Added preprocessor directives

--- double_linked_list.h
+++ double_linked_list.h
@@ -1,3 +1,6 @@
+#ifndef double_linked_list
+#define double_linked_list
+
 typedef struct double_linked_list_node{
     void *data;
     struct double_linked_list_node *left;
@@ -10,3 +13,5 @@ typedef struct double_linked_list{
 
 double_linked_list_t *get_new_double_linked_list();
 int add_data_to_double_linked_list(double_linked_list_t *dll,void *application_data);
+
+#endif // double_linked_list
