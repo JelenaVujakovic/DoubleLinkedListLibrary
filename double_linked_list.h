a4de72f513296f6850a7faaf232a3ecef8d5fc9a
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2020-12-05 08:04:49 +0100

    Add files via upload
    Header file for double linked list.

---
+++ double_linked_list.h
@@ -0,0 +1,12 @@
+typedef struct double_linked_list_node{
+    void *data;
+    struct double_linked_list_node *left;
+    struct double_linked_list_node *right;
+}double_linked_list_node_t;
+
+typedef struct double_linked_list{
+    double_linked_list_node_t *head;
+}double_linked_list_t;
+
+double_linked_list_t *get_new_double_linked_list();
+int add_data_to_double_linked_list(double_linked_list_t *dll,void *application_data);
