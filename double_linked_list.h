c99b5971753118fd4d8b339c8e0eb3c4748f0107
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2020-12-05 09:18:03 +0100

    Added prototypes of more functions

--- double_linked_list.h
+++ double_linked_list.h
@@ -14,4 +14,8 @@ typedef struct double_linked_list{
 double_linked_list_t *get_new_double_linked_list();
 int add_data_to_double_linked_list(double_linked_list_t *dll,void *application_data);
 
+int remove_data_from_double_linked_list_by_data_pointer(double_linked_list_t *dll,void *data);
+_Bool check_is_double_linked_list_empty(double_linked_list_t *dll);
+void delete_double_linked_list(double_linked_list_t *dll);
+
 #endif // double_linked_list
